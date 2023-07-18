b00d840430e6e3828837c91b03c092bc91e9e1f8
cevin <cevinbrian13@gmail.com>
2023-07-18 03:38:37 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
+int main(void)
+
+{
+	char message[] = "_putchar\n";
+write(1, message, sizeof(message) - 1);
+return (0);
+
+}
