ded08ace5a64ece596f5e6a4a46a95b035f23bb9
cevin <cevinbrian13@gmail.com>
2023-07-20 13:38:45 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,28 +1,51 @@
 #include "main.h"
+
 /**
-*print_triangle - prints a triangle
-*@size:size parameter of triangle
-*Return:returns nothing
-*/
+ * print_triangle - print a triangle at a give size.
+ * @size : size of triangle to print.
+ *
+ *
+ */
 void print_triangle(int size)
 {
-int inc1, inc2;
+/*
+ * int i: for iteration in for loop also use as a way to subtract.
+ *
+ * int margin: will recored the amount of whitesapce ill need to print.
+ * int dis"tance": is used to offset size.
+ * int to_print: is used to tell how many '#' will need to be print per-line
+ */
+	int i, margin, dis, to_print;
 
-if (size > 0)
-{
-for (inc1 = 1; inc1 <= size; inc1++)
-{
-for ((inc2 = size - inc1); inc2 > 0; inc2--)
-_putchar(' ');
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 
-for (inc2 = 0; inc2 < inc1; inc2++)
-_putchar('#');
+	dis = size - 1;
+	for (i = 0 ; i < size ; i++)
+	{
+/*
+ * we set the margin to Zero every iteration. since we're try to only print
+ * the right amount of white space. we do some math to dis to set the stopping
+ * point
+ */
+		margin = 0;
+		while (margin < (dis - i))
+		{
+			_putchar(32);
+			margin++;
+		}
 
-if (inc1 == size)
-continue;
+		to_print = size - margin;
 
-_putchar('\n');
+		while (to_print > 0)
+		{
+			_putchar('#');
+			to_print--;
+		}
+		_putchar('\n');
+	}
 }
-}
-_putchar('\n');
-}
+
