f2efd09b56f7f328aac39fcc0ef000441fcf1aa7
cevin <cevinbrian13@gmail.com>
2023-07-20 13:36:03 -0700

    Numbers have life; they're not just symbols on paper

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,46 +1,83 @@
-#include "holberton.h"
+#include "main.h"
+
+
+int _pow(int a, int b);
+
 /**
- * print_number - print an int numbers.
- * @n: number tested
- * Return: Always 0.
+ * print_number - print the given number.
+ * @n: number to print.
  */
 void print_number(int n)
 {
-	int i, j, digit, digits, power;
-	unsigned int temp, numchar, number;
+	int collection = n;
+	unsigned int new_num;
+	int count = 0;
+	int print = 0;
 
-	digit = 0;
 	if (n < 0)
 	{
+		new_num = -n;
 		_putchar('-');
-		temp = -n;
 	}
 	else
 	{
-		temp = n;
+		new_num = n;
 	}
 
-	number = temp;
+	while (collection != 0)
+	{
+		collection /= 10;
+		++count;
+	}
+	count -= 1;
 
-	while (number >= 10)
-	{
-		number = number / 10;
-		digit++;
-	}
-	digits = digit + 1;
-	power = 1;
-	i = 1;
-
-	while (i < digits)
-	{
-		power = power * 10;
-		i++;
-	}
-	j = power;
-	while (j >= 1)
-	{
-		numchar = (temp / j) % 10;
-		_putchar(numchar + '0');
-		j = j / 10;
-	}
+	do {
+		if ((new_num / (_pow(10, count)) > 9) && count > 1)
+		{
+			print = new_num / (_pow(10, count));
+			print = print % 10;
+			_putchar('0' + print);
+		}
+		else if (count > 1)
+		{
+			print = (new_num / _pow(10, count));
+		       _putchar('0' + print);
+		}
+		else if (((new_num / 10) > 9) && count == 1)
+		{
+			print = (new_num / 10) % 10;
+			_putchar('0' + print);
+		}
+		else if (count == 1)
+		{
+			print = new_num / 10;
+			_putchar('0' + print);
+		}
+		else
+		{
+			print = new_num % 10;
+			_putchar('0' + print);
+		}
+		count--;
+	} while (count > -1);
 }
+
+/**
+ * _pow - power fuction
+ * @a: value to use.
+ * @b: growth count
+ *
+ * Return: value int.
+ */
+int _pow(int a, int b)
+{
+	int i;
+	int p = a;
+
+	for (i = 1 ; i < b ; i++)
+	{
+		a *= p;
+	}
+	return (a);
+}
+
