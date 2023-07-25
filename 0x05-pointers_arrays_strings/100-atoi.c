9987bddc6188264ee3a3a4bbcb4e1f529db00dbc
cevin <cevinbrian13@gmail.com>
2023-07-25 09:07:19 -0700

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,22 +1,47 @@
 #include "main.h"
 
 /**
- * _strcpy - Copiar el contenido de uno a otra variable
- * @dest: This is destiny
- * @src: This is the copia
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- * Return: This return copy
+ * Return: the int converted from the string
  */
-char *_strcpy(char *dest, char *src)
+int _atoi(char *s)
 {
-	char *start = dest;
+	int i, d, n, len, f, digit;
 
-	while (*src != '\0')
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
 	}
-	*dest = '\0';
-	return (start);
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
+
